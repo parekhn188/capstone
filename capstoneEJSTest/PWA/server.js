f3e301650cfbb47a236558961b4a89c547473b56
Nisarg Parekh <nisargp475@gmail.com>
2021-03-10 17:01:09 -0500

    Folder for EJS framework and working web app
    Edit the .ejs files in the same way you'd edit html files. The html files aren't required anymore but kept for reference of the previous code.

--- capstoneEJSTest/PWA/server.js
+++ capstoneEJSTest/PWA/server.js
@@ -2,6 +2,7 @@
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
 
+
 //Initialize
 const app = express(); 
 const port = 3000;
--- espTest.cpp
+++ espTest.cpp
@@ -5,11 +5,14 @@
 #include <ArduinoJson.h>
 
 int userRestPos = 0; 
+int switchStatus = 0; 
+
 String strumStatus = "";  
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT); 
+  pinMode(D5, INPUT); 
 
   Serial.begin(115200); 
   Serial.println();
@@ -41,11 +44,10 @@ void loop() {
 
   int rawYData = analogRead(A0); 
   int trigger = digitalRead(D3); 
+  int switchChord = digitalRead(D5); 
+  Serial.println(switchChord);
 
   DynamicJsonDocument sendData(1024); 
-
-  //Serial.println(rawYData); 
-  //Serial.println(trigger); 
 
   // With external trigger
     if(!trigger && rawYData != userRestPos) {
@@ -53,26 +55,33 @@ void loop() {
     } else {
       strumStatus = "Idle"; 
     }
+
+    if(switchChord){
+      switchStatus = 1; 
+    } else {
+      switchStatus = 0; 
+    }
   
   //Serial.println(strumStatus); 
 
   sendData["Strum Status"] = strumStatus;
+  sendData["Switch Chord"] = switchStatus; 
 
   String postData = sendData.as<String>(); 
-  Serial.println(postData); 
+  //Serial.println(postData); 
 
   if(WiFi.status() == WL_CONNECTED) {
 
     HTTPClient http; 
     //Get request send 
-    http.begin("http://192.168.2.18:5000/sendData"); 
+    http.begin("http://192.168.2.18:3000/sendData"); 
     http.addHeader("Content-Type", "application/json"); 
 
     //int httpCode = http.POST ("{\"Strum Status\":\"Strumming\"}");
     int httpCode = http.POST (postData);
-    
 
     String payload = http.getString(); 
+    Serial.print(payload); 
 
     if (httpCode < 1) {
       Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
